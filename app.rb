23ecdb0e05a4619ea7dc764ca3b64559c14bf2c9
PhilipRoden <philip.roden@elev.ga.ntig.se>
2024-11-11 14:45:57 +0100

    11/11

--- app.rb
+++ app.rb
@@ -1,7 +1,30 @@
 class App < Sinatra::Base
+    def db
+        return @db if @db
 
+        @db = SQLite3::Database.new("db/books.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
     get '/' do
-        erb(:"index")
+        redirect('/books')
     end
 
+    get '/books' do
+        @books = db.execute('SELECT * FROM books')
+        erb(:"books/index")
+    end
+    get '/books/new' do 
+        erb(:"books/new")
+    end
+    post '/books' do
+        p params
+        titel=params["book_titel"]
+        author=params["book_author"]
+        pages=params["book_pages"]
+        status=params["book_status"]
+        db.execute("INSERT INTO books (titel, author, pages, status) VALUES(?,?,?,?)", [titel, author, pages, status])
+        redirect "/books"
+    end
 end
